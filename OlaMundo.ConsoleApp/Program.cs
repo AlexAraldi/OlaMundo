552568db942c715cc1fdee5d8791d35893836bdb
alexanderaraldi <alexanderaraldi@gmail.com>
2025-03-06 16:29:16 -0300

    Adicionar arquivos de projeto.

---
+++ OlaMundo.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OlaMundo.ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
